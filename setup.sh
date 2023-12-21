766567c44b43343f21a44c60e79b1e66ebcdc517
Zhijie Zhu <zhuzhijie1003@outlook.com>
2023-12-21 15:49:52 +0800

    Further setup script improvement

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
+pip install scipy
 git clone https://github.com/fredzzhang/detr.git
 wget https://dl.fbaipublicfiles.com/detr/detr-r50-e632da11.pth
-wget -O instances_train2017.json https://www.dropbox.com/scl/fi/k6p0aeajzzab935bxgq2v/instances_train2017.json?rlkey=qvqmn526dtu2hylcc8imxtc7s&dl=0
-wget -O hico_20160224_det.tar.gz https://www.dropbox.com/scl/fi/tpbrlpcheqa68vy06bhsa/hico_20160224_det.tar.gz?rlkey=xl5i3k3rffm1sup68nekfp0h9&dl=0
+wget -O instances_train2017.json 'https://www.dropbox.com/scl/fi/k6p0aeajzzab935bxgq2v/instances_train2017.json?rlkey=qvqmn526dtu2hylcc8imxtc7s&dl=0'
+wget -O hico_20160224_det.tar.gz 'https://www.dropbox.com/scl/fi/tpbrlpcheqa68vy06bhsa/hico_20160224_det.tar.gz?rlkey=xl5i3k3rffm1sup68nekfp0h9&dl=0'
 mkdir coco
 mkdir checkpoints
 mv instances_train2017.json coco

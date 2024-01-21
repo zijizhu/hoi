f84e5cc95ec04a92bdf952210dbe204fb7c1617e
Zhijie Zhu <zhuzhijie1003@outlook.com>
2024-01-21 13:39:59 +1100

    update training script

--- setup.sh
+++ setup.sh
@@ -1,11 +1,12 @@
 set -x
 
-pip install scipy tqdm pycocotools
+pip install -q scipy tqdm pycocotools
 git clone https://github.com/fredzzhang/detr.git
 
 ##### Download dataset  and annotation files #####
 wget -q --show-progress -O hico_20160224_det.tar.gz "https://www.dropbox.com/scl/fi/tpbrlpcheqa68vy06bhsa/hico_20160224_det.tar.gz?rlkey=xl5i3k3rffm1sup68nekfp0h9&dl=0"
 tar -xf hico_20160224_det.tar.gz --no-same-owner
+rm hico_20160224_det.tar.gz
 
 wget -q --show-progress -O trainval_hico.json "https://www.dropbox.com/scl/fi/pseygkwdglqo0fjeu3sk2/trainval_hico.json?rlkey=twx3panz8quudyla72tq0yd8x&dl=0"
 wget -q --show-progress -O test_hico.json "https://www.dropbox.com/scl/fi/hjcizyd0imfyuto2b0qrw/test_hico.json?rlkey=xi02zpv891339r2fw2xqxc1y0&dl=0"
